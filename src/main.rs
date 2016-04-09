d4ff2c7ec3909d5fa3a93875d46eaec0f902f1f2
Hossein Noroozpour <Hossein.Noroozpour@Gmail.com>
2016-04-09 15:16:39 +0430

    Primary vector.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate gtk;
 // use gtk::prelude::*
 pub mod ui;
 pub mod math;
+use math::vector::Vec;
 
 fn main() {
     let v1 = math::vector::Vec3 {
@@ -15,8 +16,21 @@ fn main() {
         y: -1.0,
         z:  1.0
     };
+    let mut v3 = math::vector::Vec3 {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0
+    };
+    let sc1 = 45.0;
 
-    println!("{:?}", (v1 + v2) * 3.9);
+    // println!("{:?}", ((v1 + v2) * 3.9).dot(v2));
+    v3 += &(&v1 + &v2);
+    println!("{:?}", &v3 * &sc1);
+    println!("{:?}", v3);
+    println!("{:?}", sc1);
+    println!("{:?}", (&v3).dot(&v1));
+    println!("{:?}", v1);
+    println!("{:?}", v3);
 
     let ui_manager = match ui::UiManager::new() {
         Some(u) => u,
--- src/math/vector.rs
+++ src/math/vector.rs
@@ -2,14 +2,20 @@ use std::ops::{
     Add,
     Sub,
     Mul,
+    Div,
     AddAssign,
     SubAssign,
-    MulAssign
+    MulAssign,
+    DivAssign
 };
 
 use std::convert::{
     Into
 };
+
+pub trait Vec <VecType, Output> {
+    fn dot(&self, o: VecType) -> Output;
+}
 
 #[derive(Debug)]
 pub struct Vec3<T> {
@@ -18,10 +24,10 @@ pub struct Vec3<T> {
     pub z: T,
 }
 
-impl<T> Add for Vec3<T> where T: Add<Output=T> {
+impl<'a, T> Add for &'a Vec3<T> where T: Add<Output=T> + Copy {
     type Output = Vec3<T>;
 
-    fn add(self, other: Vec3<T>) -> Vec3<T> {
+    fn add(self, other: &'a Vec3<T>) -> Vec3<T> {
         Vec3 {
             x: self.x + other.x,
             y: self.y + other.y,
@@ -30,18 +36,18 @@ impl<T> Add for Vec3<T> where T: Add<Output=T> {
     }
 }
 
-impl<T> AddAssign for Vec3<T> where T: AddAssign {
-    fn add_assign(&mut self, other: Vec3<T>) {
+impl<'a, T> AddAssign<&'a Vec3<T>> for Vec3<T> where T: AddAssign + Copy {
+    fn add_assign(&mut self, other: &'a Vec3<T>) {
         self.x += other.x;
         self.y += other.y;
         self.z += other.z;
     }
 }
 
-impl<T> Sub for Vec3<T> where T: Sub<Output=T> {
+impl<'a, T> Sub for &'a Vec3<T> where T: Sub<Output=T> + Copy {
     type Output = Vec3<T>;
 
-    fn sub(self, other: Vec3<T>) -> Vec3<T> {
+    fn sub(self, other: &'a Vec3<T>) -> Vec3<T> {
         Vec3 {
             x: self.x - other.x,
             y: self.y - other.y,
@@ -50,18 +56,18 @@ impl<T> Sub for Vec3<T> where T: Sub<Output=T> {
     }
 }
 
-impl<T> SubAssign for Vec3<T> where T: SubAssign {
-    fn sub_assign(&mut self, other: Vec3<T>) {
+impl<'a, T> SubAssign<&'a Vec3<T>> for Vec3<T> where T: SubAssign + Copy {
+    fn sub_assign(&mut self, other: &'a Vec3<T>) {
         self.x -= other.x;
         self.y -= other.y;
         self.z -= other.z;
     }
 }
 
-impl<T> Mul for Vec3<T> where T: Mul<Output=T> {
+impl<'a, T> Mul for &'a Vec3<T> where T: Mul<Output=T> + Copy {
     type Output = Vec3<T>;
 
-    fn mul(self, other: Vec3<T>) -> Vec3<T> {
+    fn mul(self, other: &'a Vec3<T>) -> Vec3<T> {
         Vec3 {
             x: self.x * other.x,
             y: self.y * other.y,
@@ -70,11 +76,11 @@ impl<T> Mul for Vec3<T> where T: Mul<Output=T> {
     }
 }
 
-impl<T, F> Mul<F> for Vec3<T> where T: Mul<Output=T> + Copy, F: Into<T> + Copy {
+impl<'a, 'b, T, F> Mul<&'b F> for &'a Vec3<T> where T: Mul<Output=T> + Copy, F: Into<T> + Copy {
     type Output = Vec3<T>;
 
-    fn mul(self, other: F) -> Vec3<T> {
-        let ast: T = other.into();  // I don't know compiler will do castability in compile time or in run time
+    fn mul(self, other: &'b F) -> Vec3<T> {
+        let ast: T = (*other).into();
         Vec3 {
             x: self.x * ast,
             y: self.y * ast,
@@ -83,19 +89,67 @@ impl<T, F> Mul<F> for Vec3<T> where T: Mul<Output=T> + Copy, F: Into<T> + Copy {
     }
 }
 
-impl<T> MulAssign for Vec3<T> where T: MulAssign {
-    fn mul_assign(&mut self, other: Vec3<T>) {
+impl<'a, T> MulAssign<&'a Vec3<T>> for Vec3<T> where T: MulAssign + Copy {
+    fn mul_assign(&mut self, other: &'a Vec3<T>) {
         self.x *= other.x;
         self.y *= other.y;
         self.z *= other.z;
     }
 }
 
-impl<T, F> MulAssign<F> for Vec3<T> where T: MulAssign + Copy, F: Into<T> + Copy {
-    fn mul_assign(&mut self, other: F) {
-        let ast: T = other.into();
+impl<'b, T, F> MulAssign<&'b F> for Vec3<T> where T: MulAssign + Copy, F: Into<T> + Copy {
+    fn mul_assign(&mut self, other: &'b F) {
+        let ast: T = (*other).into();
         self.x *= ast;
         self.y *= ast;
         self.z *= ast;
+    }
+}
+
+impl<'a, T> Div for &'a Vec3<T> where T: Div<Output=T> + Copy {
+    type Output = Vec3<T>;
+
+    fn div(self, other: &'a Vec3<T>) -> Vec3<T> {
+        Vec3 {
+            x: self.x / other.x,
+            y: self.y / other.y,
+            z: self.z / other.z,
+        }
+    }
+}
+
+impl<'a, 'b, T, F> Div<&'b F> for &'a Vec3<T> where T: Div<Output=T> + Copy, F: Into<T> + Copy {
+    type Output = Vec3<T>;
+
+    fn div(self, other: &'b F) -> Vec3<T> {
+        let ast: T = (*other).into();
+        Vec3 {
+            x: self.x / ast,
+            y: self.y / ast,
+            z: self.z / ast,
+        }
+    }
+}
+
+impl<'a, T> DivAssign<&'a Vec3<T>> for Vec3<T> where T: DivAssign + Copy {
+    fn div_assign(&mut self, other: &'a Vec3<T>) {
+        self.x /= other.x;
+        self.y /= other.y;
+        self.z /= other.z;
+    }
+}
+
+impl<'b, T, F> DivAssign<&'b F> for Vec3<T> where T: DivAssign + Copy, F: Into<T> + Copy {
+    fn div_assign(&mut self, other: &F) {
+        let ast: T = (*other).into();
+        self.x /= ast;
+        self.y /= ast;
+        self.z /= ast;
+    }
+}
+
+impl<'a, 'b, T> Vec<&'b Vec3<T>, T> for &'a Vec3<T> where T: Mul<Output=T> + Add<Output=T> + Copy {
+    fn dot(&self, o: &'b Vec3<T>) -> T {
+        self.x * o.x + self.y * o.y + self.z * o.z
     }
 }
