ef6d61b338ea0e365066ef1f25135635914cbc7c
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2018-05-12 14:01:49 +0430

    I'm gonna make an upheaval change in it

--- src/main.rs
+++ src/main.rs
@@ -1,227 +1,76 @@
-#[macro_use]
-extern crate conrod;
-extern crate find_folder;
+extern crate cgmath;
 extern crate image;
-extern crate num_cpus;
-extern crate rand;
-extern crate winit;
 
-pub mod math;
-pub mod render;
+use cgmath::{
+    dot,
+    Vector3,
+};
 
-use math::triangle::Triangle;
-use math::vector::{Vec2, Vec3};
-use render::camera::{Base, PerspectiveCamera};
-use render::engine::CpuEngine;
-use render::engine::Data as EngineData;
-use render::vertex::Vertex;
-
-use conrod::backend::glium::glium;
-use conrod::backend::glium::glium::Surface;
-use conrod::{color, widget, Colorable, Positionable, Sizeable, Widget};
-
-pub struct EventLoop {
-    ui_needs_update: bool,
-    last_update: std::time::Instant,
-}
-impl EventLoop {
-    pub fn new() -> Self {
-        EventLoop {
-            last_update: std::time::Instant::now(),
-            ui_needs_update: true,
-        }
-    }
-
-    /// Produce an iterator yielding all available events.
-    pub fn next(
-        &mut self,
-        events_loop: &mut glium::glutin::EventsLoop,
-    ) -> Vec<glium::glutin::Event> {
-        // We don't want to loop any faster than 60 FPS, so wait until it has been at least 16ms
-        // since the last yield.
-        let last_update = self.last_update;
-        let sixteen_ms = std::time::Duration::from_millis(16);
-        let duration_since_last_update = std::time::Instant::now().duration_since(last_update);
-        if duration_since_last_update < sixteen_ms {
-            std::thread::sleep(sixteen_ms - duration_since_last_update);
-        }
-
-        // Collect all pending events.
-        let mut events = Vec::new();
-        events_loop.poll_events(|event| events.push(event));
-
-        // If there are no events and the `Ui` does not need updating, wait for the next event.
-        if events.is_empty() && !self.ui_needs_update {
-            events_loop.run_forever(|event| {
-                events.push(event);
-                glium::glutin::ControlFlow::Break
-            });
-        }
-
-        self.ui_needs_update = false;
-        self.last_update = std::time::Instant::now();
-
-        events
-    }
-}
-
-pub fn main() {
-    const WIDTH: u32 = 1300;
-    const HEIGHT: u32 = 900;
-
-    // Build the window.
-    let mut events_loop = glium::glutin::EventsLoop::new();
-    let window = glium::glutin::WindowBuilder::new()
-        .with_title("Image Widget Demonstration")
-        .with_dimensions(WIDTH, HEIGHT);
-    let context = glium::glutin::ContextBuilder::new()
-        .with_vsync(true)
-        .with_multisampling(4);
-    let display = glium::Display::new(window, context, &events_loop).unwrap();
-
-    // construct our `Ui`.
-    let mut ui = conrod::UiBuilder::new([WIDTH as f64, HEIGHT as f64]).build();
-
-    // A type used for converting `conrod::render::Primitives` into `Command`s that can be used
-    // for drawing to the glium `Surface`.
-    let mut renderer = conrod::backend::glium::Renderer::new(&display).unwrap();
-    println!("number of cpu {:?}", num_cpus::get());
-    // The `WidgetId` for our background and `Image` widgets.
-    widget_ids!(struct Ids { background, rust_logo });
-    let ids = Ids::new(ui.widget_id_generator());
-    let vertices = vec![
-        Vertex {
-            ps: Vec3 {
-                x: -1.0,
-                y: -1.0,
-                z: 0.0,
-            },
-            nr: Vec3 {
-                x: 0.0,
-                y: 0.0,
-                z: 1.0,
-            },
-            uv: Vec2 { x: 0.0, y: 0.0 },
-        },
-        Vertex {
-            ps: Vec3 {
-                x: 0.0,
-                y: 1.0,
-                z: 0.0,
-            },
-            nr: Vec3 {
-                x: 0.0,
-                y: 0.0,
-                z: 1.0,
-            },
-            uv: Vec2 { x: 0.0, y: 0.0 },
-        },
-        Vertex {
-            ps: Vec3 {
-                x: 1.0,
-                y: -1.0,
-                z: 0.0,
-            },
-            nr: Vec3 {
-                x: 0.0,
-                y: 0.0,
-                z: 1.0,
-            },
-            uv: Vec2 { x: 0.0, y: 0.0 },
-        },
-    ];
-    let triangles = vec![Triangle::new(&[0, 1, 2], &vertices)];
-    let data = EngineData {
-        view_port_dimension: (WIDTH - 100, HEIGHT - 100),
-        samples: 1,
-        triangles: triangles,
-        vertices: vertices,
-        cameras: vec![Box::new(PerspectiveCamera::new(Base::new(
-            &Vec3 {
-                x: 0.0,
-                y: 0.0,
-                z: 2.0,
-            },
-            &Vec3 {
-                x: 0.0,
-                y: 0.0,
-                z: 0.0,
-            },
-            &Vec3 {
-                x: 0.0,
-                y: 1.0,
-                z: 0.0,
-            },
-            1.7,
-        )))],
-    };
-    // let ray = data.triangles[0].intersect(&data.cameras[0].get_ray(0.0, 0.0), 9000.0, &data.vertices);
-    // panic!("{:?}", ray);
-    let engine = CpuEngine::new(data);
-    let rust_logo = load_rust_logo(&display, &engine);
-    let (w, h) = (rust_logo.get_width(), rust_logo.get_height().unwrap());
-    println!("{:?} {:?}", w, h);
-    let mut image_map = conrod::image::Map::new();
-    let rust_logo = image_map.insert(rust_logo);
-
-    // Poll events from the window.
-    let mut event_loop = EventLoop::new();
-    'main: loop {
-        // Handle all events.
-        for event in event_loop.next(&mut events_loop) {
-            // Use the `winit` backend feature to convert the winit event to a conrod one.
-            if let Some(event) = conrod::backend::winit::convert_event(event.clone(), &display) {
-                ui.handle_event(event);
+fn main() {
+    const ASPECT: isize = 1024;
+    const HALF_ASPECT: isize = ASPECT >> 1;
+    const SAMPLES: isize = 4;
+    const SAMPLES_SQUAR: isize = SAMPLES * SAMPLES;
+    const STEP_ASPECT: f64 = 1f64 / HALF_ASPECT as f64;
+    const STEP_SAMPLE: f64 = STEP_ASPECT / SAMPLES as f64;
+    let mut pixels = [0u8; (ASPECT * ASPECT * 4) as usize];
+    let v1 = Vector3::new(-1f64, 0f64, 0f64);
+    let v2 = Vector3::new(0f64, 1f64, 0f64);
+    let v3 = Vector3::new(1f64, 0f64, 0f64);
+    let edge1 = v2 - v1;
+    let edge2 = v3 - v1;
+    let ray_origin = Vector3::new(0f64, 0f64, 2f64);
+    let mut y = 1f64;
+    let mut px_index = 0;
+    for _ in 0..ASPECT {
+        let mut x = -1f64;
+        for _ in 0..ASPECT {
+            let mut samples = [0u64; 3];
+            let mut sy = y;
+            for _ in 0..SAMPLES {
+                let mut sx = x;
+                for _ in 0..SAMPLES {
+                    let ray_dir = Vector3::new(sx, sy, -2f64);
+                    let pvec = ray_dir.cross(edge2);
+                    let det = dot(edge1, pvec);
+                    if det < 0.0001 && det > -0.0001 {
+                        sx += STEP_SAMPLE;
+                        continue;
+                    }
+                    let inv_det = 1f64 / det;
+                    let tvec = ray_origin - v1;
+                    let u = dot(tvec, pvec) * inv_det;
+                    if u < 0.0001f64 || u > 0.9999f64 {
+                        sx += STEP_SAMPLE;
+                        continue;
+                    }
+                    let qvec = tvec.cross(edge1);
+                    let v = dot(ray_dir, qvec) * inv_det;
+                    if v < 0.0001f64 || u + v > 0.9999f64 {
+                        sx += STEP_SAMPLE;
+                        continue;
+                    }
+                    let t = dot(edge2, qvec) * inv_det;
+                    if t > 0.0001 {
+                        samples[0] += 255;
+                        samples[1] += 255;
+                        samples[2] += 255;
+                    }
+                    sx += STEP_SAMPLE;
+                }
+                sy -= STEP_SAMPLE;
             }
-
-            match event {
-                glium::glutin::Event::WindowEvent { event, .. } => match event {
-                    // Break from the loop upon `Escape`.
-                    glium::glutin::WindowEvent::Closed
-                    | glium::glutin::WindowEvent::KeyboardInput {
-                        input:
-                            glium::glutin::KeyboardInput {
-                                virtual_keycode: Some(glium::glutin::VirtualKeyCode::Escape),
-                                ..
-                            },
-                        ..
-                    } => break 'main,
-                    _ => (),
-                },
-
-                _ => (),
-            }
+            pixels[px_index] = (samples[0] / SAMPLES_SQUAR as u64) as u8;
+            px_index += 1;
+            pixels[px_index] = (samples[1] / SAMPLES_SQUAR as u64) as u8;
+            px_index += 1;
+            pixels[px_index] = (samples[0] / SAMPLES_SQUAR as u64) as u8;
+            px_index += 1;
+            pixels[px_index] = 255u8;
+            px_index += 1;
+            x += STEP_ASPECT;
         }
-
-        // Instantiate the widgets.
-        {
-            let ui = &mut ui.set_widgets();
-            // Draw a light blue background.
-            widget::Canvas::new()
-                .color(color::DARK_CHARCOAL)
-                .set(ids.background, ui);
-            // Instantiate the `Image` at its full size in the middle of the window.
-            widget::Image::new(rust_logo)
-                .w_h(w as f64, h as f64)
-                .middle()
-                .set(ids.rust_logo, ui);
-        }
-
-        // Render the `Ui` and then display it on the screen.
-        if let Some(primitives) = ui.draw_if_changed() {
-            renderer.fill(&display, primitives, &image_map);
-            let mut target = display.draw();
-            target.clear_color(0.0, 0.0, 0.0, 1.0);
-            renderer.draw(&display, &mut target, &image_map).unwrap();
-            target.finish().unwrap();
-        }
+        y -= STEP_ASPECT;
     }
-}
-
-fn load_rust_logo(display: &glium::Display, engine: &CpuEngine) -> glium::texture::Texture2d {
-    let image_dimensions = engine.data.read().unwrap().view_port_dimension;
-    let image_data = engine.render();
-    let raw_image = glium::texture::RawImage2d::from_raw_rgba(image_data, image_dimensions);
-    let texture = glium::texture::Texture2d::new(display, raw_image).unwrap();
-    texture
+    image::save_buffer("image.png", &pixels, ASPECT as u32, ASPECT as u32, image::RGBA(8)).unwrap();
 }
