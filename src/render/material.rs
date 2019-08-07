09cd6757c8d85fc4050816865db910646e86e960
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2019-08-08 00:28:14 +0430

    material:: --material is complete--

--- src/render/material.rs
+++ src/render/material.rs
@@ -1,6 +1,7 @@
 use super::super::math::ray::Ray3;
 use super::super::math::vector::Vec3;
 use super::hit::Info as HitInfo;
+use rand::{thread_rng, Rng};
 
 fn schlick(cosine: f64, ref_idx: f64) -> f64 {
     let r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
@@ -12,7 +13,6 @@ pub trait Material {
     /// returns: (attenuation, scattered)
     fn scatter(&self, r_in: &Ray3, rec: &HitInfo) -> Option<(Vec3, Ray3)>;
 }
-
 
 pub struct Lambertian {
     pub albedo: Vec3,
@@ -45,7 +45,10 @@ impl Metal {
 impl Material for Metal {
     fn scatter(&self, r_in: &Ray3, rec: &HitInfo) -> Option<(Vec3, Ray3)> {
         let reflected = r_in.d.normalized().reflect(&rec.n);
-        let scattered = Ray3::new(rec.p, &reflected + &(&Vec3::random_in_unit_sphere() * self.fuzz));
+        let scattered = Ray3::new(
+            rec.p,
+            &reflected + &(&Vec3::random_in_unit_sphere() * self.fuzz),
+        );
         let attenuation = self.albedo;
         if scattered.d.dot(&rec.n) > 0.0 {
             return Some((attenuation, scattered));
@@ -66,34 +69,37 @@ impl Dielectric {
 
 impl Material for Dielectric {
     fn scatter(&self, r_in: &Ray3, rec: &HitInfo) -> Option<(Vec3, Ray3)> {
-        let mut outward_normal = Vec3::new();
-        let reflected = r_in.d.reflect(&rec.n);
-        let mut ni_over_nt = 0.0;
-        let attenuation = Vec3 { x: 1.0, y: 1.0, z: 1.0 }; 
-        let mut refracted = Vec3::new();
-        let mut reflect_prob = 0.0;
-        let mut cosine = 0.0;
-        if r_in.d.dot(&rec.n) > 0.0 {
-            outward_normal = -&rec.n;
-            ni_over_nt = self.ref_idx;
-            cosine = r_in.d.dot(&rec.n) / r_in.d.length();
-            cosine = (1.0 - self.ref_idx * self.ref_idx * ( 1.0 - cosine * cosine)).sqrt();
+        let attenuation = Vec3 {
+            x: 1.0,
+            y: 1.0,
+            z: 1.0,
+        };
+        let (outward_normal, ni_over_nt, cosine) = if r_in.d.dot(&rec.n) > 0.0 {
+            let cosine = r_in.d.dot(&rec.n) / r_in.d.length();
+            (
+                -&rec.n,
+                self.ref_idx,
+                (1.0 - self.ref_idx * self.ref_idx * (1.0 - cosine * cosine)).sqrt(),
+            )
         } else {
-            outward_normal = rec.n;
-            ni_over_nt = 1.0 / self.ref_idx;
-            cosine = -r_in.d.dot(&rec.n) / r_in.d.length();
-        }
-        let reflected = r_in.d.refract(&outward_normal, ni_over_nt);
-        if reflected.is_some() {
-            reflect_prob = schlick(cosine, self.ref_idx);
-        } else  {
-            reflect_prob = 1.0;
-        }
-        //////// todo
-        if (drand48() < reflect_prob) 
-            scattered = ray(rec.p, reflected);
-        else 
-            scattered = ray(rec.p, refracted);
-        return true;
+            (
+                rec.n,
+                1.0 / self.ref_idx,
+                -r_in.d.dot(&rec.n) / r_in.d.length(),
+            )
+        };
+        let reflected = r_in.d.reflect(&rec.n);
+        let (refracted, reflect_prob) =
+            if let Some(refracted) = r_in.d.refract(&outward_normal, ni_over_nt) {
+                (refracted, schlick(cosine, self.ref_idx))
+            } else {
+                (Vec3::new(), 1.0)
+            };
+        let scattered = if thread_rng().gen_range(-1.0f64, 1.0f64) < reflect_prob {
+            Ray3::new(rec.p, reflected)
+        } else {
+            Ray3::new(rec.p, refracted)
+        };
+        Some((attenuation, scattered))
     }
 }
